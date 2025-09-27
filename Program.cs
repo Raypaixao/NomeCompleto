d43dc0d893f23d940df06614fe52390403cf2977
rayane <rayanemedeiros1704@gmail.com>
2025-09-26 21:30:14 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.Write("digite seu primeiro nome: ");
+string Nome = Console.ReadLine();
+Console.Write("digite seu sobrenome: ");
+string Sobrenome = Console.ReadLine();
+Console.WriteLine($"Olá {Nome} {Sobrenome}");
